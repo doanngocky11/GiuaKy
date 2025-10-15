292e65c939b689a85befc83896cef4aebc6ac33d
NgocKy <kydoan309204@gmail.com>
2025-10-15 08:14:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/giuaky/activity_login.java
@@ -0,0 +1,11 @@
+package com.example.giuaky;
+
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_login  extends AppCompatActivity
+{
+    EditText edtUsername , edtPass
+    btnLogin
+}
